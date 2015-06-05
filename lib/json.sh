72d77ad61cb27792180881959c4dc9fb6390ac9a
Hunter Loftis <hunter@hunterloftis.com>
2015-06-05 11:35:38 -0400

    replaced alias with export

--- lib/json.sh
+++ lib/json.sh
@@ -12,13 +12,13 @@ get_cpu() {
 
 os=$(get_os)
 cpu=$(get_cpu)
-alias jq "$BP_DIR/vendor/jq-$os"
+export JQ="$BP_DIR/vendor/jq-$os"
 
 read_json() {
   local file=$1
   local key=$2
   if test -f $file; then
-    cat $file | jq --raw-output "$key // \"\"" || return 1
+    cat $file | $JQ --raw-output "$key // \"\"" || return 1
   else
     echo ""
   fi
